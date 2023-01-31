7231d82b1bf4378b0f6b6f9571c39cdc7cad401f
sanyaman <119109046+sanyaman@users.noreply.github.com>
2023-01-31 16:20:50 +0300

    rev2

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+let popup = document.querySelector(".popup"); // переиенная модалки
+let popupOpen = document.querySelector(".profile__button-edit"); //переменная карандаша
+let popupClose = document.querySelector(".popup__close"); // переменная  кнопки закрыть
+let popupSumbit = document.querySelector(".popup__sumbit"); //переменная кнопки сохранить
+let popupFill = document.querySelector(".popup__fill"); //переменная полей формы из нее взять  name
+let profileTitle = document.querySelector(".profile__title"); // переменная  профиля имя
+let profileSubtitle = document.querySelector(".profile__subtitle"); // переменная   профиля описания
+let formElement = document.querySelector(".popup__form"); // переменная формы
+// let nameInput = formElement.elements.name; // переменная выбор имя поля (тоже правильно!!!)
+// let jobInput = formElement.elements.description; //переменная  выбор имя поля (тоже правильно!!!)
+let nameInput = document.querySelector(".popup__fill_name_value");
+let jobInput = document.querySelector(".popup__fill_description_value");
+//функция открытия попап
+function openPopup() {
+  popup.classList.add("popup_opened");
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
+//функция закрытия попап
+function closePopup() {
+  popup.classList.remove("popup_opened");
+  formElement.reset();
+}
+//функция редактирования формы
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  closePopup();
+}
+// открытие , закрытие , сохранение
+popupOpen.addEventListener("click", openPopup);
+popupClose.addEventListener("click", closePopup);
+formElement.addEventListener("submit", handleFormSubmit);
